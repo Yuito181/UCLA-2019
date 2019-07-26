a0a4a66cdbace6324e76543c044e562f3d53b075
Yuito181 <yuito.tomihara181@gmail.com>
2019-07-26 13:05:09 -0700

    learn about if and else etc

---
+++ 2-variables.py
@@ -0,0 +1,12 @@
+color = input("What is your favorite color?")
+if color == "Blue":
+    print("Nice! Me too!")
+elif color =="Red":
+    print("Meh.That is ok.")
+    country = input("Hmm,what country do you wanna visit?")
+    if country == "Italy" or country =="France":
+        print("kkkkkkk")
+    else:
+        print("It is a terrible choice?('")
+else:
+    print("wwwwwww")    
